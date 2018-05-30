8a90ec606988ae6dafc0340a60c418ec2140f092
Ned Vogel <nedvogel@Neds-MacBook-Air.local>
2018-05-29 23:33:36 -0700

    homework commit

---
+++ app.js
@@ -0,0 +1,6 @@
+var topics = [
+    "Heavy Metal", "New Wave Of British Heavy Metal", "Speed Metal", "Hair Metal", "Doom Metal", "Grindcore","Thrash Metal", "Stoner Metal", "Nu Metal", "Black Metal"
+];
+for ( i = 0;i < topics.length; i++) {
+    console.log(topics[i]);
+}
